9360bff4a095e53bef70f62e0b058ee630e8b813
Виктор <vibelow@mail.ru>
2024-04-04 00:24:18 +0300

    PeopleAndBooks

---
+++ src/main/java/ru/below/springbootproject/dao/PersonDAO.java
@@ -0,0 +1,7 @@
+package ru.below.springbootproject.dao;
+
+import org.springframework.stereotype.Component;
+@Component
+public class PersonDAO {
+    // Здесь будут лежать специфические запросы к БД (с помощью SQL)
+}
